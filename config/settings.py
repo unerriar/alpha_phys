f2fe9897156551341e9eadfba4ede821c65e1d9c
Unerriar <unerriar@gmail.com>
2025-09-09 21:10:03 +0300

    Library fix

--- config/settings.py
+++ config/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-go289k3av_7!xz(2_7q6l&8d5@^_jf=*co)y$5jntg=%)mtsvt'
-
+# SECRET_KEY = 'django-insecure-go289k3av_7!xz(2_7q6l&8d5@^_jf=*co)y$5jntg=%)mtsvt'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['*']
 
