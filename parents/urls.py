36864f176186c786223103bfe9a6fba16eb4856c
Unerriar <34584431+unerriar@users.noreply.github.com>
2026-03-06 21:14:07 +0300

    Delete parents directory

--- parents/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('about/', views.about, name='parents_about'),
-    path('individual/', views.individual, name='parents_individual'),
-    path('groups/', views.groups, name='parents_groups'),
-    path('selfstudy/', views.selfstudy, name='parents_selfstudy'),
-]
